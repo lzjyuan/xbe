84cf1cacd76a8f33faf05891aa5a02467fcc36f8
lzjyuan <44632246+lzjyuan@users.noreply.github.com>
2024-06-05 22:21:36 +0800

    Update xbn.js

--- xbn.js
+++ xbn.js
@@ -2,16 +2,15 @@
 @zjy
 日期:2022.04.03
 [rewrite_local]
-^https:\/\/xiaobien-api\.baobaobooks\.com\/bookshelf\/v1\/v3_3_0\/booktab\/play\/1011773 url script-response-body https://raw.githubusercontent.com/lzjyuan/xbe/main/xbn.js
+^https:\/\/xiaobien-api\.baobaobooks\.com\/bookshelf\/v1\/v3_3_0\/books\/1011773 url script-response-body https://raw.githubusercontent.com/lzjyuan/xbe/main/xbn.js
 hostname = xiaobien-api.baobaobooks.com
 *********************************************/
 
-//https://xiaobien-api.baobaobooks.com/bookshelf/v1/v3_3_0/booktab/play/1011773
+//https://xiaobien-api.baobaobooks.com/bookshelf/v1/v3_3_0/booktab/books/1011773
 
 var obj = JSON.parse($response.body);
 //obj.is_buy = 5;
-//obj.is_user_buy=true;
-//obj.book_number = "姐姐";
-//obj.book_brief = "gvccchjjn";
-obj.show_type = 0;
+obj.is_user_buy=1;
+obj.is_suit=1
+obj.book_name = "zjy";
 $done({body : JSON.stringify(obj)});
